c569f2ea64e8063c7d46708f2b24114bbd538f32
Samrit Mukherjee <samritmukherjee05@gmail.com>
2026-07-14 20:49:33 +0530

    Binary Search

--- BinarySearch.c
+++ BinarySearch.c
@@ -10,9 +10,9 @@ int bin_src(int arr[], int low, int high, int key)
     if (arr[mid] == key)
         return mid;
     else if (key < arr[mid])
-        return binarySearch(arr, low, mid - 1, key);
+        return bin_src(arr, low, mid - 1, key);
     else
-        return binarySearch(arr, mid + 1, high, key);
+        return bin_src(arr, mid + 1, high, key);
 }
 
 int main()
@@ -33,7 +33,7 @@ int main()
     printf("Enter element to search: ");
     scanf("%d", &key);
 
-    result = binarySearch(arr, 0, n - 1, key);
+    result = bin_src(arr, 0, n - 1, key);
 
     if(result == -1)
         printf("Element not found.");
